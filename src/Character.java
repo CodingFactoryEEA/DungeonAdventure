9e6d1cbe85f3acf42252004a35142fd2864f69f3
Rdjaouzi <riadh.djaouzi@gmail.com>
2023-10-25 16:07:41 +0200

    first commit

---
+++ src/Character.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+public class Character {
+
+    private int m_iLifePoints;
+    /**
+     * Default constructor
+     */
+    public Character() {
+    }
+
+    public void attack(Character p_TargetCharacter){
+
+    }
+
+    public int getM_iLifePoints() {
+        return m_iLifePoints;
+    }
+
+    public void receiveDamages(int p_iDamages){
+        m_iLifePoints -= p_iDamages;
+    }
+
+    public boolean isalive(){
+        return m_iLifePoints > 0 ;
+    }
+}
