9c3fb23c9f34d79290edb1ef79cfd3215c82186f
Rdjaouzi <riadh.djaouzi@gmail.com>
2023-10-25 21:52:24 +0200

    Mise à jours du code et Documentation

--- src/MonsterWeapon.java
+++ src/MonsterWeapon.java
@@ -1,6 +1,15 @@
 /**
  * 
  */
-public class MonsterWeapon{
+public class MonsterWeapon extends Weapon {
+
+    private int m_iCriticalCountDown;
+    private static final int s_iMaxRoundBeforeCritical = 5;
+
+    @Override
+    public void inflictDamage(Character p_TargetCharacter) {
+        super.inflictDamage(p_TargetCharacter);
+        // TODO: 25/10/2023
+    }
 
 }
