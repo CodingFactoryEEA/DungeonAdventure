e67b9bb2653d650b0b9d374347f16d835729b295
EA <eden.agbo@outlook.fr>
2023-10-25 21:20:34 +0200

    Filling Dungeon, Item, StrengthPotion, HealthPotion, HeroWeapons (partially) classes with fields and methods skeleton.

--- src/HeroWeapons.java
+++ src/HeroWeapons.java
@@ -3,5 +3,9 @@
  */
 public class HeroWeapons extends Weapon implements Searchable {
 
-
+//TODO: Implements method
+    @Override
+    public Item search() {
+        return null;
+    }
 }
--- src/Item.java
+++ src/Item.java
@@ -2,6 +2,9 @@
  * 
  */
 public abstract class Item {
-
-
+    public int m_iEffectValue;
+    public abstract int applyEffect(Hero p_Hero);
+    public void setValue(int p_iValue){
+        //TODO: Implement function
+    };
 }
