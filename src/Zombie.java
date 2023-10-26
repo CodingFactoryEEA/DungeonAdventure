34d463e69f90dffde97b6a0c2859e79bba33e151
Rdjaouzi <riadh.djaouzi@gmail.com>
2023-10-27 00:56:47 +0200

    Initialisation les point de vie du Zombie dans le constructeur

--- src/Zombie.java
+++ src/Zombie.java
@@ -7,6 +7,7 @@ public class Zombie extends Monster {
      * Default constructor
      */
     public Zombie() {
+        m_iLifePoints = ConfigParams.m_iLifePointsZombie;
         super.m_sEffectiveWeaponType = Bite.class.getName();
         super.m_Weapon = new Bite();
         if (Monster.s_iNbZombieInstances % 5 == 1) {
