6491cade9f0e90c52d3cffdcc9b5f4980a5dbe8a
Rdjaouzi <riadh.djaouzi@gmail.com>
2023-10-26 15:11:52 +0200

    mise a jours des méthodes isWeaponEfficient et getEffectiveWeaponType et ajours des param m_sEffectiveWeaponType et m_Item

--- src/Monster.java
+++ src/Monster.java
@@ -9,6 +9,10 @@ public class Monster extends Character implements Searchable {
     protected static int s_iNbBarbarianInstances = 0;
     protected static int s_iNbTrollInstances = 0;
 
+    private String m_sEffectiveWeaponType;
+
+    protected Item m_Item;
+
 
     /**
      * @return
@@ -18,12 +22,25 @@ public class Monster extends Character implements Searchable {
         return null;
     }
 
+    /**
+     * Cette méthode permet de recupérer l'arme éfficace.
+     * @return m_sEffectiveWeaponType
+     */
+    public String getEffectiveWeaponType() {
+        return m_sEffectiveWeaponType;
+    }
+
     @Override
     public void attack(Character p_TargetCharacter) {
 
     }
+
+    /**
+     * Cette méthode permet d'indiquer si l'arme est efficace contre le monstre.
+     * @param p_Weapon
+     * @return
+     */
     public boolean isWeaponEfficient(Weapon p_Weapon){
-        //TODO Implements method
-        return true;
+        return this.m_Weapon.equals(p_Weapon);
     }
 }
